c329771e3c2859890c8ad397fdef4da06d4fd95e
Jon Chen <bsd@voltaire.sh>
2020-11-19 06:12:16 +0000

    cli

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/Yamashou/gqlgenc v0.0.0-20201118110422-c5e4e29019a7
+	github.com/kelseyhightower/envconfig v1.4.0
 	golang.org/x/tools v0.0.0-20201118215654-4d9c4f8a78b0 // indirect
 )
+
+replace github.com/kelseyhightower/envconfig v1.4.0 => github.com/bsdlp/envconfig v1.5.0
--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/bsdlp/what-im-watching/twitch"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	OAuthToken        string `split_words:"true" required:"true"`
+	ClientId          string `split_words:"true" required:"true"`
+	TwitchGqlEndpoint string `split_words:"true" default:"https://gql.twitch.tv/gql"`
+}
+
 //go:generate go run github.com/Yamashou/gqlgenc
 func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := twitch.NewClient(http.DefaultClient, cfg.TwitchGqlEndpoint, setAuth(cfg.OAuthToken), setClientId(cfg.ClientId))
+	ctx := context.Background()
+	currentlyWatching, err := client.GetCurrentlyWatching(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if currentlyWatching.CurrentUser == nil {
+		log.Fatal("current user is nil")
+	}
+	if currentlyWatching.CurrentUser.Activity == nil {
+		log.Println("not currently watching anything")
+	}
+	log.Printf("%s is currently watching %s", currentlyWatching.DisplayName, currentlyWatching.CurrentUser.Activity.User.DisplayName)
+}
+
+func setAuth(oauthToken string) func(*http.Request) {
+	return func(req *http.Request) {
+		req.Header.Set("Authorization", "OAuth "+oauthToken)
+	}
+}
+
+func setClientId(clientId string) func(*http.Request) {
+	return func(req *http.Request) {
+		req.Header.Set("Client-Id", clientId)
+	}
 }
