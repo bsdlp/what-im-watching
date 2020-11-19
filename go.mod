dba95b78f9990dfa7a92fa9d92ee428db8e977e9
Jon Chen <bsd@voltaire.sh>
2020-11-19 06:40:18 +0000

    tweet about it

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/Yamashou/gqlgenc v0.0.0-20201118110422-c5e4e29019a7
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
+	github.com/dghubble/oauth1 v0.6.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	golang.org/x/tools v0.0.0-20201118215654-4d9c4f8a78b0 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/bsdlp/what-im-watching/twitch"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
-	OAuthToken        string `envconfig:"OAUTH_TOKEN" required:"true"`
-	ClientId          string `split_words:"true" required:"true"`
-	TwitchGqlEndpoint string `split_words:"true" default:"https://gql.twitch.tv/gql"`
+	OAuthToken          string `envconfig:"OAUTH_TOKEN" required:"true"`
+	ClientId            string `split_words:"true" required:"true"`
+	TwitchGqlEndpoint   string `split_words:"true" default:"https://gql.twitch.tv/gql"`
+	TwitterApiKey       string `required:"true" split_words:"true"`
+	TwitterApiSecret    string `required:"true" split_words:"true"`
+	TwitterAccessToken  string `required:"true" split_words:"true"`
+	TwitterAccessSecret string `required:"true" split_words:"true"`
 }
 
 //go:generate go run github.com/Yamashou/gqlgenc
@@ -22,6 +29,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	twitterOAuthConfig := oauth1.NewConfig(cfg.TwitterApiKey, cfg.TwitterApiSecret)
+	twitterToken := oauth1.NewToken(cfg.TwitterAccessToken, cfg.TwitterAccessSecret)
+	twitterClient := twitter.NewClient(twitterOAuthConfig.Client(oauth1.NoContext, twitterToken))
 
 	client := twitch.NewClient(http.DefaultClient, cfg.TwitchGqlEndpoint, setAuth(cfg.OAuthToken), setClientId(cfg.ClientId))
 	ctx := context.Background()
@@ -33,10 +44,21 @@ func main() {
 		log.Fatal("current user is nil")
 	}
 	if currentlyWatching.CurrentUser.Activity == nil {
-		log.Printf("%s is not currently watching anything", currentlyWatching.CurrentUser.DisplayName)
+		msg := fmt.Sprintf("%s is not currently watching anything", currentlyWatching.CurrentUser.DisplayName)
+		_, _, err = twitterClient.Statuses.Update(msg, nil)
+		if err != nil {
+			log.Printf("error posting: %s", err)
+		}
+		log.Println(msg)
 		return
 	}
-	log.Printf("%s is currently watching %s", currentlyWatching.CurrentUser.DisplayName, currentlyWatching.CurrentUser.Activity.User.DisplayName)
+
+	msg := fmt.Sprintf("%s is currently watching %s", currentlyWatching.CurrentUser.DisplayName, currentlyWatching.CurrentUser.Activity.User.DisplayName)
+	_, _, err = twitterClient.Statuses.Update(msg, nil)
+	if err != nil {
+		log.Printf("error posting: %s", err)
+	}
+	log.Println(msg)
 }
 
 func setAuth(oauthToken string) func(*http.Request) {
